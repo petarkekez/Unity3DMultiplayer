bfae92bc4e27350de55cdf4afdd6394f51bc73cf
Petar Kekez <petar.kekez@outlook.com>
2016-09-29 12:46:33 +0200

    FINAL TUTORIAL COMMIT
    FINAL TUTORIAL COMMIT

--- Unity3DMultiplayer/Assets/Scripts/Health.cs
+++ Unity3DMultiplayer/Assets/Scripts/Health.cs
@@ -13,6 +13,17 @@ public class Health : NetworkBehaviour
 
     public bool destroyOnDeath;
 
+    private NetworkStartPosition[] spawnPoints;
+
+
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+    }
+
     public void TakeDamage(int amount)
     {
         if (!isServer)
@@ -44,8 +55,15 @@ public class Health : NetworkBehaviour
     {
         if (isLocalPlayer)
         {
-            transform.position = Vector3.zero;
-            //currentHealth = maxHealth;
+            if (spawnPoints != null && spawnPoints.Length > 0)
+            {
+                var spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+                transform.position = spawnPoint;
+            }
+            else
+            {
+                transform.position = Vector3.zero;
+            }
         }
     }
 }
