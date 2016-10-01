1442c5e76900003c5b8e6c5295ff83920f959554
Petar Kekez <petar.kekez@outlook.com>
2016-10-01 17:50:58 +0200

    Enabled mobile app and added fireRate
    Enabled mobile app and added fireRate

--- Unity3DMultiplayer/Assets/Scripts/PlayerController.cs
+++ Unity3DMultiplayer/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,10 @@ public class PlayerController : NetworkBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
-    
+
+    private DateTime lastBulletFired = DateTime.Now;
+    private const int fireRatePerSecond = 4;
+    private TimeSpan minTimeBetweenSohots = new TimeSpan(TimeSpan.TicksPerSecond / fireRatePerSecond);
 
     public override void OnStartLocalPlayer()
     {
@@ -26,14 +29,44 @@ public class PlayerController : NetworkBehaviour
         if (!isLocalPlayer)
             return;
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        float x = 0;
+        float z = 0;
+
+        if (Application.isMobilePlatform)
+        {
+            x = (Input.acceleration.x) * Time.deltaTime * 150.0f;
+            z = (-1 * Input.acceleration.z - 0.75f) * Time.deltaTime * 10.0f;
+            if (z > 5) z = 5;
+            if (z < -5) z = -5;
+        }
+        else
+        {
+            x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        }
+        
 
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
 
-        if(Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0)
+        bool fire = false;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            fire = true;
+
+        if(Input.touches.Length > 0)
+            foreach (var touch in Input.touches)
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    fire = true;
+                    break;
+                }
+            }
+
+        if(fire &&  DateTime.Now - lastBulletFired > minTimeBetweenSohots)
         {
+            lastBulletFired = DateTime.Now;
             CmdFire();
         }
         
